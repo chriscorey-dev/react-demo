505c7366323e6050952e513b3aa35e8df36996a8
Chris Corey <chriscorey.dev@gmail.com>
2018-10-17 16:06:25 -0700

    Adding functionality and styling. Pulling from api, able to get and put.

--- src/pages/sakila/Sakila.jsx
+++ src/pages/sakila/Sakila.jsx
@@ -1,8 +1,138 @@
 import React, { Component } from "react";
 
+// TODO: Setup authentication for posts for public api built on back end.
+
 class Sakila extends Component {
+  state = {
+    url: "http://localhost:3001/api/sakila/film",
+    // const url = "https://api.chris-corey.com/api/sakila/film";
+    data: null
+  };
+
+  componentDidMount() {
+    document.title = "Sakila";
+
+    fetch(this.state.url)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          data: response
+        });
+      });
+  }
+
   render() {
-    return <div className="container">Sakila</div>;
+    const { data, url } = this.state;
+
+    if (!data) {
+      return (
+        <div className="container">
+          <p>Loading Films...</p>
+        </div>
+      );
+    }
+
+    const getFilm = id => {
+      // TODO: Validate parameters
+      // let useUrl = null;
+      // if (id == null) {
+      //   useUrl = `${url}/${id}`;
+      // }
+
+      fetch(url)
+        .then(response => response.json())
+        .then(response =>
+          this.setState({
+            data: response
+          })
+        );
+    };
+
+    const putFilm = (id, body) => {
+      // TODO: Validate parameters
+      // if (body == {}) return false;
+
+      fetch(`${url}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify(body)
+      }).then(response => getFilm());
+    };
+
+    const postFilm = body => {
+      fetch(`${url}/${id}`, {
+        // method: "POST",
+        // headers: {
+        //   "Content-Type": "application/json; charset=utf-8"
+        // },
+        // body: JSON.stringify(body)
+      })
+        .then(console.log("delete!"))
+        .then(response => getFilm());
+    };
+
+    const deleteFilm = id => {
+      // TODO: Validate parameters
+      // if (body == {}) return false;
+
+      fetch(`${url}/${id}`, {
+        // method: "DELETE"
+      })
+        .then(console.log("delete!"))
+        .then(response => getFilm());
+    };
+    return (
+      <div className="container">
+        <button
+          className="btn btn-outline-primary btn-sm mr-1"
+          onClick={() => {
+            getFilm;
+          }}
+        >
+          GET
+        </button>
+        <button
+          className="btn btn-outline-success btn-sm mr-1"
+          onClick={() => {
+            postFilm;
+          }}
+        >
+          POST
+        </button>
+        <ul className="list-group list-group-flush mt-2">
+          {data.map(film => (
+            <li key={film.film_id} className="list-group-item">
+              {film.title}
+              <button
+                className="btn btn-outline-danger btn-sm float-right mr-1"
+                onClick={() => {
+                  const confirm = window.confirm(
+                    `Are you sure you want to delete film: ${film.title}`
+                  );
+                  if (confirm) {
+                    deleteFilm(film.film_id);
+                  }
+                }}
+              >
+                DELETE
+              </button>
+              <button
+                className="btn btn-outline-primary btn-sm float-right mr-1"
+                onClick={() => {
+                  const title = prompt("Enter title", film.title);
+                  putFilm(film.film_id, { title: title });
+                }}
+              >
+                PUT
+              </button>
+            </li>
+          ))}
+        </ul>
+        {/* <p>{JSON.stringify(data)}</p> */}
+      </div>
+    );
   }
 }
 
